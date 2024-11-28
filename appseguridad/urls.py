8ab5fc7d846095589dc9a7c5900fb6ff8fc92fe3
nicoalexanderdev <nicoalexanderdev@gmail.com>
2024-11-27 21:07:00 -0300

    login funcionando

--- appseguridad/urls.py
+++ appseguridad/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.user_login, name='login'),
+    path('home', views.home, name='home'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- appseguridad/views.py
+++ appseguridad/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'app/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            messages.success(request, 'Inicio de sesión exitoso')
+            return redirect('home')
+        else:
+            messages.error(
+                request, 'Credenciales inválidas. Inténtalo de nuevo.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'app/login.html', {"form": form})
+
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'app/home.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
